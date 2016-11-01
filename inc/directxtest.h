8df4aa9e4b926c6fba9b5b6b8cc7b1e5d29785a3
Chuck Walbourn <chuckw@windows.microsoft.com>
2016-11-01 16:04:05 -0700

    updated media path

--- inc/directxtest.h
+++ inc/directxtest.h
@@ -44,6 +44,6 @@
                                        digest[0], digest[1], digest[2], digest[3], digest[4], digest[5], digest[6], digest[7], \
                                        digest[8], digest[9], digest[10], digest[11], digest[12], digest[13], digest[14], digest[15] );
 
-#define MEDIA_PATH L"\\\\durfs\\durango\\TestContent\\graphics\\directxtex\\media\\"
+#define MEDIA_PATH L"D:\\Microsoft\\directxtexmedia\\"
 
 #define TEMP_PATH L"%TEMP%\\"
