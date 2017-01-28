90a0058f5e369f14942451a6be94135baba60eaf
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-01-28 14:54:29 -0800

    Fuzz testing

---
+++ fuzzloaders/fuzzloaders.cpp
@@ -0,0 +1,432 @@
+//--------------------------------------------------------------------------------------
+// File: fuzzloaders.cpp
+//
+// Simple command-line tool for fuzz-testing for image loaders
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// http://go.microsoft.com/fwlink/?LinkId=248929
+//--------------------------------------------------------------------------------------
+
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+#pragma warning(pop)
+
+#include <windows.h>
+
+#include <memory>
+#include <list>
+
+#include "DirectXTex.h"
+
+namespace
+{
+    struct handle_closer { void operator()(HANDLE h) { if (h) CloseHandle(h); } };
+
+    typedef public std::unique_ptr<void, handle_closer> ScopedHandle;
+
+    inline HANDLE safe_handle(HANDLE h) { return (h == INVALID_HANDLE_VALUE) ? 0 : h; }
+
+    struct find_closer { void operator()(HANDLE h) { assert(h != INVALID_HANDLE_VALUE); if (h) FindClose(h); } };
+
+    typedef public std::unique_ptr<void, find_closer> ScopedFindHandle;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+enum OPTIONS
+{
+    OPT_RECURSIVE = 1,
+    OPT_DDS,
+    OPT_HDR,
+    OPT_TGA,
+    OPT_WIC,
+    OPT_MAX
+};
+
+static_assert(OPT_MAX <= 32, "dwOptions is a DWORD bitfield");
+
+struct SConversion
+{
+    wchar_t szSrc[MAX_PATH];
+};
+
+struct SValue
+{
+    LPCWSTR pName;
+    DWORD dwValue;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+const SValue g_pOptions [] =
+{
+    { L"r",         OPT_RECURSIVE },
+    { L"dds",       OPT_DDS },
+    { L"hdr",       OPT_HDR },
+    { L"tga",       OPT_TGA },
+    { L"wic",       OPT_WIC },
+    { nullptr,      0 }
+};
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+namespace
+{
+#pragma prefast(disable : 26018, "Only used with static internal arrays")
+
+    DWORD LookupByName(const wchar_t *pName, const SValue *pArray)
+    {
+        while (pArray->pName)
+        {
+            if (!_wcsicmp(pName, pArray->pName))
+                return pArray->dwValue;
+
+            pArray++;
+        }
+
+        return 0;
+    }
+
+    const wchar_t* LookupByValue(DWORD pValue, const SValue *pArray)
+    {
+        while (pArray->pName)
+        {
+            if (pValue == pArray->dwValue)
+                return pArray->pName;
+
+            pArray++;
+        }
+
+        return L"";
+    }
+
+    void SearchForFiles(const wchar_t* path, std::list<SConversion>& files, bool recursive)
+    {
+        // Process files
+        WIN32_FIND_DATA findData = {};
+        ScopedFindHandle hFile(safe_handle(FindFirstFileExW(path,
+            FindExInfoBasic, &findData,
+            FindExSearchNameMatch, nullptr,
+            FIND_FIRST_EX_LARGE_FETCH)));
+        if (hFile)
+        {
+            for (;;)
+            {
+                if (!(findData.dwFileAttributes & (FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_DIRECTORY)))
+                {
+                    wchar_t drive[_MAX_DRIVE] = {};
+                    wchar_t dir[_MAX_DIR] = {};
+                    _wsplitpath_s(path, drive, _MAX_DRIVE, dir, _MAX_DIR, nullptr, 0, nullptr, 0);
+
+                    SConversion conv;
+                    _wmakepath_s(conv.szSrc, drive, dir, findData.cFileName, nullptr);
+                    files.push_back(conv);
+                }
+
+                if (!FindNextFile(hFile.get(), &findData))
+                    break;
+            }
+        }
+            
+        // Process directories
+        if (recursive)
+        {
+            wchar_t searchDir[MAX_PATH] = {};
+            {
+                wchar_t drive[_MAX_DRIVE] = {};
+                wchar_t dir[_MAX_DIR] = {};
+                _wsplitpath_s(path, drive, _MAX_DRIVE, dir, _MAX_DIR, nullptr, 0, nullptr, 0);
+                _wmakepath_s(searchDir, drive, dir, L"*", nullptr);
+            }
+
+            hFile.reset(safe_handle(FindFirstFileExW(searchDir,
+                FindExInfoBasic, &findData,
+                FindExSearchLimitToDirectories, nullptr,
+                FIND_FIRST_EX_LARGE_FETCH)));
+            if (!hFile)
+                return;
+
+            for (;;)
+            {
+                if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+                {
+                    if (findData.cFileName[0] != L'.')
+                    {
+                        wchar_t subdir[MAX_PATH] = {};
+
+                        {
+                            wchar_t drive[_MAX_DRIVE] = {};
+                            wchar_t dir[_MAX_DIR] = {};
+                            wchar_t fname[_MAX_FNAME] = {};
+                            wchar_t ext[_MAX_FNAME] = {};
+                            _wsplitpath_s(path, drive, dir, fname, ext);
+                            wcscat_s(dir, findData.cFileName);
+                            _wmakepath_s(subdir, drive, dir, fname, ext);
+                        }
+
+                        SearchForFiles(subdir, files, recursive);
+                    }
+                }
+
+                if (!FindNextFile(hFile.get(), &findData))
+                    break;
+            }
+        }
+    }
+
+    void PrintUsage()
+    {
+        wprintf(L"Usage: fuzzloaders <options> <files>\n");
+        wprintf(L"\n");
+        wprintf(L"   -r                  wildcard filename search is recursive\n");
+        wprintf(L"   -dds                force use of DDS loader\n");
+        wprintf(L"   -hdr                force use of HDR loader\n");
+        wprintf(L"   -tga                force use of TGA loader\n");
+        wprintf(L"   -wic                force use of WIC loader\n");
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+//--------------------------------------------------------------------------------------
+// Entry-point
+//--------------------------------------------------------------------------------------
+#pragma prefast(disable : 28198, "Command-line tool, frees all memory on exit")
+
+int __cdecl wmain(_In_ int argc, _In_z_count_(argc) wchar_t* argv[])
+{
+    // Initialize COM (needed for WIC)
+    HRESULT hr = hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr))
+    {
+        wprintf(L"Failed to initialize COM (%08X)\n", hr);
+        return 1;
+    }
+
+    // Process command line
+    DWORD dwOptions = 0;
+    std::list<SConversion> conversion;
+
+    for (int iArg = 1; iArg < argc; iArg++)
+    {
+        PWSTR pArg = argv[iArg];
+
+        if (('-' == pArg[0]) || ('/' == pArg[0]))
+        {
+            pArg++;
+            PWSTR pValue;
+
+            for (pValue = pArg; *pValue && (':' != *pValue); pValue++);
+
+            if (*pValue)
+                *pValue++ = 0;
+
+            DWORD dwOption = LookupByName(pArg, g_pOptions);
+
+            if (!dwOption || (dwOptions & (1 << dwOption)))
+            {
+                PrintUsage();
+                return 1;
+            }
+
+            dwOptions |= 1 << dwOption;
+
+            switch (dwOption)
+            {
+            case OPT_DDS:
+            case OPT_HDR:
+            case OPT_TGA:
+            case OPT_WIC:
+                {
+                    DWORD mask = (1 << OPT_DDS) | (1 << OPT_HDR) | (1 << OPT_TGA) | (1 << OPT_WIC);
+                    mask &= ~(1 << dwOption);
+                    if (dwOptions & mask)
+                    {
+                        wprintf(L"-dds, -hdr, -tga, and -wic are mutually exclusive options\n");
+                        return 1;
+                    }
+                }
+                break;
+            }
+        }
+        else if (wcspbrk(pArg, L"?*") != nullptr)
+        {
+            size_t count = conversion.size();
+            SearchForFiles(pArg, conversion, (dwOptions & (1 << OPT_RECURSIVE)) != 0);
+            if (conversion.size() <= count)
+            {
+                wprintf(L"No matching files found for %ls\n", pArg);
+                return 1;
+            }
+        }
+        else
+        {
+            SConversion conv;
+            wcscpy_s(conv.szSrc, MAX_PATH, pArg);
+
+            conversion.push_back(conv);
+        }
+    }
+
+    if (conversion.empty())
+    {
+        wprintf(L"ERROR: Need at least 1 image file to fuzz\n\n");
+        PrintUsage();
+        return 0;
+    }
+
+    for (auto pConv = conversion.begin(); pConv != conversion.end(); ++pConv)
+    {
+        wchar_t ext[_MAX_EXT];
+        _wsplitpath_s(pConv->szSrc, nullptr, 0, nullptr, 0, nullptr, 0, ext, _MAX_EXT);
+        bool isdds = (_wcsicmp(ext, L".dds") == 0);
+        bool ishdr = (_wcsicmp(ext, L".hdr") == 0);
+        bool istga = (_wcsicmp(ext, L".tga") == 0);
+
+        bool usedds = false;
+        bool usehdr = false;
+        bool usetga = false;
+        if (dwOptions & (1 << OPT_DDS))
+        {
+            usedds = true;
+        }
+        else if (dwOptions & (1 << OPT_TGA))
+        {
+            usetga = true;
+        }
+        else if (dwOptions & (1 << OPT_HDR))
+        {
+            usehdr = true;
+        }
+        else if (!(dwOptions & (1 << OPT_WIC)))
+        {
+            usedds = isdds;
+            usehdr = ishdr;
+            usetga = istga;
+        }
+
+        // Load source image
+#ifdef _DEBUG
+        OutputDebugStringW(pConv->szSrc);
+        OutputDebugStringA("\n");
+#endif
+
+        DirectX::ScratchImage result;
+        if (usedds)
+        {
+            hr = DirectX::LoadFromDDSFile(pConv->szSrc, DirectX::DDS_FLAGS_NONE, nullptr, result);
+            if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+            {
+                wprintf(L"ERROR: DDSTexture file not not found:\n%ls\n", pConv->szSrc);
+                return 1;
+            }
+            else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != E_OUTOFMEMORY && hr != HRESULT_FROM_WIN32(ERROR_HANDLE_EOF) && (hr != E_FAIL || (hr == E_FAIL && isdds)))
+            {
+#ifdef _DEBUG
+                char buff[128] = {};
+                sprintf_s(buff, "DDSTexture failed with %08X\n", hr);
+                OutputDebugStringA(buff);
+#endif
+                wprintf(L"!");
+            }
+            else
+            {
+                wprintf(SUCCEEDED(hr) ? L"*" : L".");
+            }
+        }
+        else if (usehdr)
+        {
+            hr = DirectX::LoadFromHDRFile(pConv->szSrc, nullptr, result);
+            if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+            {
+                wprintf(L"ERROR: HDRTexture file not not found:\n%ls\n", pConv->szSrc);
+                return 1;
+            }
+            else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != E_OUTOFMEMORY && hr != HRESULT_FROM_WIN32(ERROR_HANDLE_EOF) && (hr != E_FAIL || (hr == E_FAIL && ishdr)))
+            {
+#ifdef _DEBUG
+                char buff[128] = {};
+                sprintf_s(buff, "HDRTexture failed with %08X\n", hr);
+                OutputDebugStringA(buff);
+#endif
+                wprintf(L"!");
+            }
+            else
+            {
+                wprintf(SUCCEEDED(hr) ? L"*" : L".");
+            }
+        }
+        else if (usetga)
+        {
+            hr = DirectX::LoadFromTGAFile(pConv->szSrc, nullptr, result);
+            if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+            {
+                wprintf(L"ERROR: TGATexture file not not found:\n%ls\n", pConv->szSrc);
+                return 1;
+            }
+            else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != E_OUTOFMEMORY && hr != HRESULT_FROM_WIN32(ERROR_HANDLE_EOF) && (hr != E_FAIL || (hr == E_FAIL && istga)))
+            {
+#ifdef _DEBUG
+                char buff[128] = {};
+                sprintf_s(buff, "TGATexture failed with %08X\n", hr);
+                OutputDebugStringA(buff);
+#endif
+                wprintf(L"!");
+            }
+            else
+            {
+                wprintf(SUCCEEDED(hr) ? L"*" : L".");
+            }
+        }
+        else
+        {
+            hr = DirectX::LoadFromWICFile(pConv->szSrc, DirectX::WIC_FLAGS_NONE, nullptr, result);
+            if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+            {
+                wprintf(L"ERROR: WICTexture file not found:\n%ls\n", pConv->szSrc);
+                return 1;
+            }
+            else if (FAILED(hr) && hr != E_INVALIDARG && hr != HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED) && hr != WINCODEC_ERR_COMPONENTNOTFOUND && hr != E_OUTOFMEMORY && hr != WINCODEC_ERR_BADHEADER)
+            {
+#ifdef _DEBUG
+                char buff[128] = {};
+                sprintf_s(buff, "WICTexture failed with %08X\n", hr);
+                OutputDebugStringA(buff);
+#endif
+                wprintf(L"!");
+            }
+            else
+            {
+                wprintf(SUCCEEDED(hr) ? L"*" : L".");
+            }
+        }
+        fflush(stdout);
+    }
+
+    wprintf(L"\n");
+
+    return 0;
+}
