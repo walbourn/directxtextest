5cb7465c740748a3ba7ac0c90c459a0ed9f25fd0
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-06-05 14:25:26 -0700

    Initial commit

---
+++ platformtest/pch.h
@@ -0,0 +1,63 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+#include <wrl.h>
+
+#include <d3d11_3.h>
+
+#if defined(NTDDI_WIN10_RS2)
+#include <dxgi1_6.h>
+#else
+#include <dxgi1_5.h>
+#endif
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include <algorithm>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+
+#include <stdio.h>
+#include <pix.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+namespace DX
+{
+    // Helper class for COM exceptions
+    class com_exception : public std::exception
+    {
+    public:
+        com_exception(HRESULT hr) : result(hr) {}
+
+        virtual const char* what() const override
+        {
+            static char s_str[64] = {};
+            sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
+    // Helper utility converts D3D API failures into exceptions.
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            throw com_exception(hr);
+        }
+    }
+}
