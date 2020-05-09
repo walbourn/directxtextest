4cf9c7e3e46c2bb836f484ec766301a56b3811c4
Chuck Walbourn <chuckw@microsoft.com>
2020-05-09 11:36:50 -0700

    Added standalone test to clang validate the DDSTextureLoader, ScreenGrab, WICTextureLoader

---
+++ standalonetest/test11.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This test ensures that all public headers fully include all their dependancies, as well compile cleanly at maximum warning level
+
+#include "DDSTextureLoader11.h"
+#include "ScreenGrab11.h"
+#include "WICTextureLoader11.h"
+
+void test11()
+{
+    return;
+}
---
+++ standalonetest/test12.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This test ensures that all public headers fully include all their dependancies, as well compile cleanly at maximum warning level
+
+#include "DDSTextureLoader12.h"
+#include "ScreenGrab12.h"
+#include "WICTextureLoader12.h"
+
+void test12()
+{
+    return;
+}
