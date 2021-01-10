1873e1361f1a1c594d1fa2ae58b50b9bda0baf22
Chuck Walbourn <chuckw@windows.microsoft.com>
2021-01-09 16:47:57 -0800

    More tests for texassemble

---
+++ toolscripts/texassemble/t-merge.cmd
@@ -0,0 +1,17 @@
+@echo off
+call startup.cmd %0
+
+echo .
+echo merge -nologo -f rgba -o %OUTTESTDIR%\merge.bmp %REFDIR%\fishingboat.jpg %REFDIR%\lena.jpg 
+"%RUN%" merge -nologo -f rgba -o %OUTTESTDIR%\merge.bmp %REFDIR%\fishingboat.jpg %REFDIR%\lena.jpg 
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
+
+echo .
+echo merge -nologo -f rgba -o %OUTTESTDIR%\merge2.bmp -swizzle RGBg %REFDIR%\fishingboat.jpg %REFDIR%\lena.jpg 
+"%RUN%" merge -nologo -f rgba -o %OUTTESTDIR%\merge2.bmp -swizzle RGBg %REFDIR%\fishingboat.jpg %REFDIR%\lena.jpg 
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
+
+echo .
+echo merge -nologo -f rgba -o %OUTTESTDIR%\merge3.bmp -swizzle GGGr %REFDIR%\fishingboat.jpg %REFDIR%\lena.jpg 
+"%RUN%" merge -nologo -f rgba -o %OUTTESTDIR%\merge3.bmp -swizzle GGGr %REFDIR%\fishingboat.jpg %REFDIR%\lena.jpg 
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
