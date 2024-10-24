3d4ba0294565406542d0266a20b27c24f4fea5e1
Chuck Walbourn <chuckw@microsoft.com>
2024-10-24 12:45:55 -0700

    Added values test

--- toolscripts/texconv/t-long-options.cmd
+++ toolscripts/texconv/t-long-options.cmd
@@ -15,3 +15,18 @@ echo.
 echo -nologo --file-type HDR -o %OUTTESTDIR% --overwrite --format R16G16B16A16_FLOAT --prefix LO_ %REFDIR%\1234-bmp.bmp
 %RUN% -nologo --file-type HDR -o %OUTTESTDIR% --overwrite --format R16G16B16A16_FLOAT --prefix LO_ %REFDIR%\1234-bmp.bmp
 if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
+
+echo.
+echo -nologo --width 100 --height 50 -o %OUTTESTDIR% --suffix _100x50a %REFDIR%\1234-bmp.bmp
+%RUN% -nologo --width 100 --height 50 -o %OUTTESTDIR% --suffix _100x50a %REFDIR%\1234-bmp.bmp
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
+
+echo.
+echo -nologo --width=100 --height=50 -o %OUTTESTDIR% --suffix _100x50b %REFDIR%\1234-bmp.bmp
+%RUN% -nologo --width=100 --height=50 -o %OUTTESTDIR% --suffix _100x50b %REFDIR%\1234-bmp.bmp
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
+
+echo.
+echo -nologo --width:100 --height:50 -o %OUTTESTDIR% --suffix _100x50c %REFDIR%\1234-bmp.bmp
+%RUN% -nologo --width:100 --height:50 -o %OUTTESTDIR% --suffix _100x50c %REFDIR%\1234-bmp.bmp
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
